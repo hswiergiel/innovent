58a74f0a49bebfb31f46923c8e29820e175fe50f
Hervé Swiergiel <herve.swiergiel@JPVELOWKS016.elosi.lan>
2023-08-16 16:43:26 +0200

    initial commit

---
+++ services/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { User } from './user/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Herve62300?',
+      database: 'innovent',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Liste des Utilisateurs</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <strong>{{ user.username }}</strong> - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get('http://localhost:3000/users'); // Assurez-vous que le chemin correspond à votre API
+        this.users = response.data;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des utilisateurs :', error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+/* Styles si nécessaire */
+</style>
